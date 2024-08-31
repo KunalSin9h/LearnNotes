899c2520730aceca2d792b1e68acceb41b66a057
Kunal Singh <kunalsin9h@gmail.com>
2024-08-31 15:32:22 +0000

    updated og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,28 @@ export const metadata: Metadata = {
   title: "Technocrats TNP Cell Essay Writing",
   description: "Technocrats TNP Cell Essay Writing",
   icons: "/favicon.png",
+
+  publisher: "Kunal Singh",
+  authors: [{ name: "Kunal Singh", url: "https://kunalsin9h.com" }],
+  colorScheme: "light",
+  openGraph: {
+    title: "TNP Cell Essay Writing",
+    description: "Technocrats TNP Cell Essay Writing",
+    url: "https://tnpcell.vercel.app",
+    siteName: "Essay Practice",
+    images: [
+      { url: "https://i.imgur.com/e3jFZNC.jpeg", width: 1200, height: 650 },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "TNP Cell Essay Writing",
+    images: {
+      url: "https://i.imgur.com/e3jFZNC.jpeg",
+    },
+  }
 };
 
 export default function RootLayout({
